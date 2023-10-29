a7606d59547613fca27ac70577dfd20698985cc4
anubhav ladha <anubhav@appeq.ai>
2023-10-29 23:01:09 +0530

    fix

---
+++ src/ImageDisplay.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function ImageDisplay() {
+  const [imageUrl, setImageUrl] = useState(null);
+
+  useEffect(() => {
+    // Fetch the user's image from your server
+    const fetchUserImage = async () => {
+      try {
+        const response = await axios.get('/user-image');
+        setImageUrl(URL.createObjectURL(new Blob([response.data])));
+      } catch (error) {
+        console.error('Error fetching user image:', error);
+        console.log('Error response:', error.response); // Log the response details
+      }
+    };
+
+    fetchUserImage();
+  }, []);
+
+  return (
+    <div>
+      {imageUrl && <img src={imageUrl} alt="User" />}
+    </div>
+  );
+}
+
+export default ImageDisplay;
