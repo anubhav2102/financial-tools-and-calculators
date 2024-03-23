1083254ff86955ca8c14e7c268370f9e209264c3
anubhav ladha <anubhav@appeq.ai>
2024-03-24 00:13:43 +0530

    gpt bugs fixes

--- src/components/dashboardLayout/AI/ChatGPTScreen.jsx
+++ src/components/dashboardLayout/AI/ChatGPTScreen.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 const ChatGPTScreen = ({currentItem}) => {
     let [allChats, setAllChats] = useState([]);
     const [inputValue, setInputValue] = useState('');
+    let [promptAutoCount, setPromptAutoCount] = useState(-1);
 
     const handleInputChange = (event) => {
         setInputValue(event.target.value);
@@ -23,10 +24,12 @@ const ChatGPTScreen = ({currentItem}) => {
                 alert('Please login to continue');
                 return;
             }
+            setPromptAutoCount(-1);
             const response = await axios.post('http://localhost:8000/api/v1/generate-response', { 
                 prompt: prompt, 
                 email: email, 
-                stockDetail: currentItem
+                stockDetail: currentItem,
+                autoPromptCount: promptAutoCount
              });
             console.log(response);
             setAllChats([]);
@@ -67,71 +70,96 @@ const ChatGPTScreen = ({currentItem}) => {
         const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
         return `${diffDays} day${diffDays > 1 ? 's' : ''} ago`;
     }
-    useEffect(()=>{
-        const getAutoPrompt = async () => {
-            if(localStorage.getItem('email_id') && currentItem){
-                let email = localStorage.getItem('email_id');
-                        let prompt = `Generate insights about ${currentItem} stock performance and outlook based on recent data.`;
-                        console.log(prompt)
+    const getAutoPrompt = async () => {
+        if(localStorage.getItem('email_id') && currentItem &&
+         ((JSON.parse(localStorage.getItem('gptGenerated')) && JSON.parse(localStorage.getItem('gptGenerated')).length>0 && !JSON.parse(localStorage.getItem('gptGenerated')).includes(currentItem) )
+         || !JSON.parse(localStorage.getItem('gptGenerated')))){
+            setPromptAutoCount(++promptAutoCount);
+            let email = localStorage.getItem('email_id');
+                    let prompt = `Generate insights about ${currentItem} stock performance and outlook based on recent data.`;
+                    console.log(prompt)
+                    try {
                         const response = await axios.post('http://localhost:8000/api/v1/generate-response', { 
-                            prompt: prompt, 
-                            email: email, 
-                            stockDetail: currentItem 
-                        });
-                        console.log(response);
-                        setAllChats([]);
-            if(response.data.code===200){
-                let allChatsData = [];
-                let idx = -1;
-                    for(let i=0;i<response.data.data.stockData.length;i++){
-                        if(response.data.data.stockData[i].stockDetail===currentItem){
+                        prompt: prompt, 
+                        email: email, 
+                        stockDetail: currentItem,
+                        autoPromptCount: promptAutoCount
+                    });
+                    console.log(response);
+                    setAllChats([]);
+        if(response.data.code===200){
+            let data = [];
+            if(localStorage.getItem('gptGenerated')){
+                data = JSON.parse(localStorage.getItem('gptGenerated'));
+            }
+            if(!data.includes(currentItem)){
+                data.push(currentItem);
+            }
+            localStorage.setItem('gptGenerated', JSON.stringify(data));
+            let allChatsData = [];
+            let idx = -1;
+                for(let i=0;i<response.data.data.stockData.length;i++){
+                    if(response.data.data.stockData[i].stockDetail===currentItem){
+                        idx = i;
+                        break;
+                    }
+                }
+                if(idx!==-1){
+                    for(let i=0;i<response.data.data.stockData[idx].prompt.length;i++){
+                        allChatsData.push({prompt: response.data.data.stockData[idx].prompt[i], response: response.data.data.stockData[idx].response[i], timeStamp: response.data.data.stockData[idx].timestamp[i]});
+                    }
+                }
+            setAllChats(allChatsData);
+        }
+                    } catch (error) {
+                        console.error(error);
+                    }
+        }
+    }
+    const getChatsData = async () => {
+        try {
+            let email = localStorage.getItem('email_id');
+            if(email){
+                let data = await axios.post('http://localhost:8000/api/v1/get-previous-chats', {
+                    email: email
+                });
+                console.log(data);
+                if(data.data.code===200){
+                    let allChatsData = [];
+                    let idx = -1;
+                    for(let i=0;i<data.data.data.stockData.length;i++){
+                        if(data.data.data.stockData[i].stockDetail===currentItem){
                             idx = i;
                             break;
                         }
                     }
                     if(idx!==-1){
-                        for(let i=0;i<response.data.data.stockData[idx].prompt.length;i++){
-                            allChatsData.push({prompt: response.data.data.stockData[idx].prompt[i], response: response.data.data.stockData[idx].response[i], timeStamp: response.data.data.stockData[idx].timestamp[i]});
+                        for(let i=0;i<data.data.data.stockData[idx].prompt.length;i++){
+                            allChatsData.push({prompt: data.data.data.stockData[idx].prompt[i], response: data.data.data.stockData[idx].response[i], timeStamp: data.data.data.stockData[idx].timestamp[i]});
                         }
-                    }
-                setAllChats(allChatsData);
-            }
-            }
-        }
-        const getChatsData = async () => {
-            try {
-                let email = localStorage.getItem('email_id');
-                if(email){
-                    let data = await axios.post('http://localhost:8000/api/v1/get-previous-chats', {
-                        email: email
-                    });
-                    console.log(data);
-                    if(data.data.code===200){
-                        let allChatsData = [];
-                        let idx = -1;
-                        for(let i=0;i<data.data.data.stockData.length;i++){
-                            if(data.data.data.stockData[i].stockDetail===currentItem){
-                                idx = i;
-                                break;
-                            }
-                        }
-                        if(idx!==-1){
-                            for(let i=0;i<data.data.data.stockData[idx].prompt.length;i++){
-                                allChatsData.push({prompt: data.data.data.stockData[idx].prompt[i], response: data.data.data.stockData[idx].response[i], timeStamp: data.data.data.stockData[idx].timestamp[i]});
-                            }
-                            setAllChats(allChatsData);
-                        }else{
-                            getAutoPrompt();
-                        }
+                        setAllChats(allChatsData);
+                    }else{
+                        getAutoPrompt();
                     }
                 }
-            } catch (error) {
-                console.error(error);
-                getAutoPrompt();
             }
+        } catch (error) {
+            console.error(error);
         }
-        getChatsData();
-    }, [currentItem]);
+    }
+    const checkStatus = () => {
+        if(JSON.parse(localStorage.getItem('gptGenerated')) && JSON.parse(localStorage.getItem('gptGenerated')).length>0 && JSON.parse(localStorage.getItem('gptGenerated')).includes(currentItem)){
+            console.log('code going in get requesr')
+            getChatsData();
+        }else{
+            console.log('code going in post requesr')
+            getAutoPrompt();
+        }
+    }
+    useEffect(()=>{
+        checkStatus();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
         <>
